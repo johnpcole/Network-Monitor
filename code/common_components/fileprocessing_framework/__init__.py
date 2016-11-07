a7547eeb7f38d3f8d3540c79f6df7b085943d417
Dr J P Cole <johnpcole@users.noreply.github.com>
2016-11-07 12:39:24 +0000

    Updated file processing framework (Version 5.1)

--- code/common_components/fileprocessing_framework/__init__.py
+++ code/common_components/fileprocessing_framework/__init__.py
@@ -1,1 +1,7 @@
+# Version 5.1
+
+# - readfromdisk simplified
+# - tabulateddata, commadata, datapair added
+
+# ====================================================================================
 # Version 5.0
--- code/common_components/fileprocessing_framework/fileprocessing_module.py
+++ code/common_components/fileprocessing_framework/fileprocessing_module.py
@@ -1,27 +1,40 @@
-# ---------------------------------------------------------
-# Opens and reads a file into a simple string list
-# ---------------------------------------------------------
-
 def readfromdisk(filename):
 
-	outcome = []
+	newfilelist = []
 
 	try:
 		# Open the file for the duration of this process
 		with open(filename) as fileobject:
-			
+
 			# Loop over all lines in the file
 			for fileline in fileobject.readlines():
-				
-				# Only add data if the line isn't blank
+
+				# Only process if the line isn't blank
 				if fileline != "":
-				
-					# Attempt to extract useful information, if possible
-					outcome.append(fileline.rstrip('\r\n'))
+					newfilelist.append(fileline.rstrip('\r\n'))
 
 	except:
 		# Print an error if the file cannot be read
-		print "Cannot read file - ", filename
-		
-	return outcome
+		print "Cannot read file - Configs/Field.txt"
 
+	return newfilelist
+
+	
+	
+def tabulateddata(fileline):
+
+	splitdata = fileline.split("\t")
+	return splitdata
+
+
+
+def commadata(fileline):
+	splitdata = fileline.split(", ")
+	return splitdata
+
+
+
+def datapair(dataitem):
+
+	splitdata = dataitem.split(" = ")
+	return splitdata[0], splitdata[1]
