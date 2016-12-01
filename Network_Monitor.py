387390050bd7bd1a618df4bfa6983efe89a064db
Dr J P Cole <johnpcole@users.noreply.github.com>
2016-12-01 14:16:25 +0000

    Fixed merge issue AGAIN!

--- Network_Monitor.py
+++ Network_Monitor.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 from codebase import main_application as NetworkMonitor
-=======
-from code import main_application as NetworkMonitor
->>>>>>> refs/remotes/origin/master
 
 
 print "Application Started"
